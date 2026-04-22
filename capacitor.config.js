9ca88b4dd0b798362126c7f9248f21bc2c045a28
tbiller3 <t.biller3@gmail.com>
2026-04-22 02:14:59 -0600

    Update capacitor.config.js via POSTAPP

--- capacitor.config.js
+++ capacitor.config.js
@@ -1,13 +1,18 @@
-/** @type {import("@capacitor/cli").CapacitorConfig} */
+/** @type {import('@capacitor/cli').CapacitorConfig} */
 const config = {
-  appId: "com.tbiller.postapp",
-  appName: "POSTAPP",
-  webDir: "www",
+  appId: 'com.tbiller.postapp',
+  appName: 'POSTAPP',
+  webDir: 'www',
+  server: {
+    url: 'https://postapp.replit.app',
+    cleartext: false,
+    allowNavigation: [],
+  },
   ios: {
-    backgroundColor: "#000000",
-    statusBarStyle: "lightContent",
-    minimumOsVersion: "15.0",
-    contentInset: "automatic",
+    backgroundColor: '#0d1117',
+    statusBarStyle: 'lightContent',
+    minimumOsVersion: '15.0',
+    contentInset: 'automatic',
   },
 };
 
